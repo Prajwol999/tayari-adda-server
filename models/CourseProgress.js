61041fff1b4095e8d05d7c217d93453c0635fbca
Prajwol999 <130135344+Prajwol999@users.noreply.github.com>
2025-12-05 19:44:41 +0545

    feat:order part

--- models/CourseProgress.js
+++ models/CourseProgress.js
@@ -1,17 +1,17 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const LectureProgressSchema = new mongoose.Schema({
-//   lectureId: String,
-//   viewed: Boolean,
-//   dateViewed: Date,
-// });
+const LectureProgressSchema = new mongoose.Schema({
+  lectureId: String,
+  viewed: Boolean,
+  dateViewed: Date,
+});
 
-// const CourseProgressSchema = new mongoose.Schema({
-//   userId: String,
-//   courseId: String,
-//   completed: Boolean,
-//   completionDate: Date,
-//   lecturesProgress: [LectureProgressSchema],
-// });
+const CourseProgressSchema = new mongoose.Schema({
+  userId: String,
+  courseId: String,
+  completed: Boolean,
+  completionDate: Date,
+  lecturesProgress: [LectureProgressSchema],
+});
 
-// module.exports = mongoose.model("Progress", CourseProgressSchema);
+module.exports = mongoose.model("Progress", CourseProgressSchema);

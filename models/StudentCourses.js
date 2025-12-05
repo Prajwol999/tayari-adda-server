2e9a92b9c0b0d4b7d1b1d7795d1f365b29de7594
Prajwol999 <130135344+Prajwol999@users.noreply.github.com>
2025-12-05 19:46:23 +0545

    course route solved

--- models/StudentCourses.js
+++ models/StudentCourses.js
@@ -1,17 +1,17 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const StudentCoursesSchema = new mongoose.Schema({
-//   userId: String,
-//   courses: [
-//     {
-//       courseId: String,
-//       title: String,
-//       instructorId: String,
-//       instructorName: String,
-//       dateOfPurchase: Date,
-//       courseImage: String,
-//     },
-//   ],
-// });
+const StudentCoursesSchema = new mongoose.Schema({
+  userId: String,
+  courses: [
+    {
+      courseId: String,
+      title: String,
+      instructorId: String,
+      instructorName: String,
+      dateOfPurchase: Date,
+      courseImage: String,
+    },
+  ],
+});
 
-// module.exports = mongoose.model("StudentCourses", StudentCoursesSchema);
+module.exports = mongoose.model("StudentCourses", StudentCoursesSchema);

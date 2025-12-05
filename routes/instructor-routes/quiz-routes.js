879b00fd7e948b93638cefa5d36c634ecf75cfb6
Prajwol999 <130135344+Prajwol999@users.noreply.github.com>
2025-12-05 19:49:51 +0545

    feat: implement quiz management and student course progress API routes

--- routes/instructor-routes/quiz-routes.js
+++ routes/instructor-routes/quiz-routes.js
@@ -1,11 +1,11 @@
-// const express = require("express");
-// const { createQuizSet, getAllQuizSets, getQuizSetsByCategory,deleteQuizSet } = require("../../controllers/instructor-controller/quiz-controller");
+const express = require("express");
+const { createQuizSet, getAllQuizSets, getQuizSetsByCategory,deleteQuizSet } = require("../../controllers/instructor-controller/quiz-controller");
 
-// const router = express.Router();
+const router = express.Router();
 
-// router.post("/create", createQuizSet);
-// router.get("/", getAllQuizSets);
-// router.get("/category/:category", getQuizSetsByCategory);
-// router.delete("/delete/:id", deleteQuizSet);
+router.post("/create", createQuizSet);
+router.get("/", getAllQuizSets);
+router.get("/category/:category", getQuizSetsByCategory);
+router.delete("/delete/:id", deleteQuizSet);
 
-// module.exports = router;
+module.exports = router;
